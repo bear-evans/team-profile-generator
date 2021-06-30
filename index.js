e165ded9e2c0da2cad0a9365239dc72c6ed5d23a
bear-evans <bear-evans@outlook.com>
2021-06-30 19:10:46 -0400

    Added blank tests and basic code framework

---
+++ index.js
@@ -0,0 +1,20 @@
+// ================================
+// Requirements and Imports
+// ================================
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// ================================
+// Team Roster Module
+// ================================
+const teamRoster = function () {
+  function init() {}
+
+  function queryUser() {}
+
+  function writeFile() {}
+
+  return {
+    init: init,
+  };
+};
